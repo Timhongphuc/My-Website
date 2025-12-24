be6d5fc7b1a15e3932d6f1315b68bfe70c141e83
Tim Seufert <116903809+Timhongphuc@users.noreply.github.com>
2025-12-24 14:56:36 +0100

    fixed typo (git is going crazy today lol)

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,13 +12,13 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gianmarcocavallo.com/",
+  site: "https://timhonpghuc.de/",
   integrations: [
     sitemap(),
     robotsTxt({
       sitemap: [
-        "https://gianmarcocavallo.com/sitemap-index.xml",
-        "https://gianmarcocavallo.com/sitemap-0.xml",
+        "https://timhonpghuc.de/sitemap-index.xml",
+        "https://timhonpghuc.de/sitemap-0.xml",
       ],
     }),
     solidJs(),
