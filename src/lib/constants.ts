6cf70c52c81c60170955566a14272b988dc4e9ce
Tim Seufert <116903809+Timhongphuc@users.noreply.github.com>
2026-03-14 12:18:05 +0100

    Link generated for GOOGLE DRIVE CV new.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,7 +5,7 @@ export const LINKS = {
   discord: "https://discordapp.com/users/1088801571132952616",
   tumo: "https://portfolio.de.tumo.world/p/lqg1d406zry9vwnx16z1k52pn7xmow3v",
   email: "contact@gianmarcocavallo.com<",
-  cv: "https://drive.google.com/file/d/1pepoDA9jOw5eJZn5ADBXLVqp43wQOhah/view?usp=sharing"
+  cv: "https://drive.google.com/file/d/1fZ2oVba-BkcRtnbeUy9M6Tfx16gpFKSg/view?usp=share_link"
 };
 
 export const loaderAnimation = [
