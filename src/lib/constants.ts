b5b346389353ea0da085b8c2eec428cd38dfdaa0
Tim Seufert <116903809+Timhongphuc@users.noreply.github.com>
2026-07-15 00:09:00 +0700

    Replaced my Medium link with substack

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 export const LINKS = {
   github: "https://github.com/Timhongphuc",
   googleDevs: "https://g.dev/timhongphuc",
-  medium: "https://medium.com/@Tiefentechnik",
+  substack: "https://substack.com/@timhongphuc",
   discord: "https://discordapp.com/users/1088801571132952616",
   tumo: "https://portfolio.de.tumo.world/p/lqg1d406zry9vwnx16z1k52pn7xmow3v",
   email: "contact@gianmarcocavallo.com<",
